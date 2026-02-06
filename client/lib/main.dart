c6dbb6ef8742034b10210e830ba64a10c5f65bb8
Akshit-101 <akshit.p@kalvium.community>
2026-02-06 22:55:00 +0530

    feat: integrated Firebase SDKs via FlutterFire CLI

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,3 +1,4 @@
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'screens/splash_screen.dart';
@@ -11,9 +12,12 @@ import 'screens/create_account_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
+
 
 
 class MyApp extends StatelessWidget {
